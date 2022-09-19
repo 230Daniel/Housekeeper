596dcc2fff1b8ae8bca8008fd5d1950c91fd153a
Daniel Baynton <daniel.baynton@hotmail.com>
2022-09-19 23:12:02 +0100

    Added initial database migration

---
+++ Housekeeper/Migrations/20220919175825_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Housekeeper.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "jobs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    days_of_week = table.Column<int>(type: "integer", nullable: false),
+                    week_frequency = table.Column<int>(type: "integer", nullable: false),
+                    user_ids = table.Column<decimal[]>(type: "numeric(20,0)[]", nullable: true),
+                    previous_user_index = table.Column<int>(type: "integer", nullable: false),
+                    start_of_first_week_midday = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_jobs", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "jobs");
+        }
+    }
+}
